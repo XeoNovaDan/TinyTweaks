e167222f7d8abc6c6a6f7d3a31a6c0fbeb4cf11d
XeoNovaDan <toghill7252010@hotmail.co.uk>
2019-07-16 22:15:29 +0100

    Fiddled with quality gen code again

--- Source/TinyTweaks/HarmonyPatches/Patch_QualityUtility.cs
+++ Source/TinyTweaks/HarmonyPatches/Patch_QualityUtility.cs
@@ -56,50 +56,51 @@ namespace TinyTweaks
             {
                 if (TinyTweaksSettings.changeQualityDistribution)
                 {
-                    switch(relevantSkillLevel)
+                    // It's messy but designed to be as non-destructive as possible
+                    switch (relevantSkillLevel)
                     {
                         case 0:
-                            return 0.2f; // 0.7 => 0 (Awful)
+                            return originalQuality - 0.7f; // 0.7 => 0 (Awful)
                         case 1:
-                            return 0.5f; // 1.1 => 0.33
+                            return originalQuality - 0.77f; // 1.1 => 0.33
                         case 2:
-                            return 0.8f; // 1.5 => 0.67
+                            return originalQuality - 0.83f; // 1.5 => 0.67
                         case 3:
-                            return 1; // 1.8 => 1 (Poor)
+                            return originalQuality - 0.8f; // 1.8 => 1 (Poor)
                         case 4:
-                            return 1.4f; // 2 => 1.33
+                            return originalQuality - 0.67f; // 2 => 1.33
                         case 5:
-                            return 1.7f; // 2.2 => 1.67
+                            return originalQuality - 0.53f; // 2.2 => 1.67
                         case 6:
-                            return 2; // 2.4 => 2 (Normal)
+                            return originalQuality - 0.4f; // 2.4 => 2 (Normal)
                         case 7:
-                            return 2.25f; // 2.6 => 2.25
+                            return originalQuality - 0.35f; // 2.6 => 2.25
                         case 8:
-                            return 2.5f; // 2.8 => 2.5
+                            return originalQuality - 0.3f; // 2.8 => 2.5
                         case 9:
-                            return 2.75f; // 2.95 => 2.75
+                            return originalQuality - 0.2f; // 2.95 => 2.75
                         case 10:
-                            return 3; // 3.1 => 3 (Good)
+                            return originalQuality - 0.1f; // 3.1 => 3 (Good)
                         case 11:
-                            return 3.25f; // 3.25 => 3.25
+                            return originalQuality; // 3.25 => 3.25
                         case 12:
-                            return 3.5f; // 3.4 => 3.5
+                            return originalQuality + 0.1f; // 3.4 => 3.5
                         case 13:
-                            return 3.75f; // 3.5 => 3.75
+                            return originalQuality + 0.25f; // 3.5 => 3.75
                         case 14:
-                            return 4; // 3.6 => 4 (Excellent)
+                            return originalQuality + 0.4f; // 3.6 => 4 (Excellent)
                         case 15:
-                            return 4.15f; // 3.7 => 4.15
+                            return originalQuality + 0.45f; // 3.7 => 4.15
                         case 16:
-                            return 4.3f; // 3.8 => 4.3
+                            return originalQuality + 0.5f; // 3.8 => 4.3
                         case 17:
-                            return 4.45f; // 3.9 => 4.45
+                            return originalQuality + 0.55f; // 3.9 => 4.45
                         case 18:
-                            return 4.6f; // 4 => 4.6
+                            return originalQuality + 0.6f; // 4 => 4.6
                         case 19:
-                            return 4.75f; // 4.1 => 4.75
+                            return originalQuality + 0.65f; // 4.1 => 4.75
                         case 20:
-                            return 5; // 4.2 => 5 (Masterwork)
+                            return originalQuality + 0.8f; // 4.2 => 5 (Masterwork)
                     }
                 }
                 return originalQuality;
